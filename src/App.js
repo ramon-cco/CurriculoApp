5ed2ed12d3590c8df2601f1d03989eae8476ceb7
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2022-02-22 16:15:18 -0300

    update index

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {StyleSheet, View, Image, Text, bold} from 'react-native';
+import foto from './assets/foto.jpg';
+// import Icon from 'react-native-vector-icons/Feather';
 
-import foto from './assets/fotoPerfil.jpg'
 
 const App = () => {
   return (
     <>
-    <View style={style.page}>
-      <View style={style.container_cabecalho}>
-        <Image source={foto} style={style.foto}/>
+      <View style={style.page}>
+        <View style={style.container_cabecalho}>
+          <Image source={foto} style={style.foto} />
+          <Text style={style.nome}>RAMON OLIVEIRA</Text>
+          <Text style={style.funcao}>Dev Mobile</Text>
+          <View>
+            {/* <Icon name="github" /> */}
+            <Text>Linker</Text>
+            <Text></Text>
+          </View>
+        </View>
       </View>
-    </View>
-    </>
-  )
-  
+  </>
+  );
 };
-
 
 const style = StyleSheet.create({
   page: {
-    backgroundColor: `#E7E7E7`,
-    flex:1,
+    backgroundColor: '#E7E7E7',
+    flex: 1,
   },
-  container_cabecalho:{
+  container_cabecalho: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   foto: {
     width: 250,
-    height:250,
+    height: 250,
     borderRadius: 125,
-
-
   },
-
+  nome: {
+      fontSize: 26,
+      fontWeight: bold,
+      marginTop: 10
+    },
+    funcao: {
+      color: '#939393',
+      marginBottom: 10
+    }
 })
 
 export default App;
