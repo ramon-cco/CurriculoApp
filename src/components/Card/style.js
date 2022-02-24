73ae17bb7802ba720322062431766a1f5862c997
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2022-02-24 00:36:49 -0300

    Componetizando app

---
+++ src/components/Card/style.js
@@ -0,0 +1,27 @@
+
+import { StyleSheet } from 'react-native';
+
+const Style = StyleSheet.create({
+
+    card_container: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+      },
+      card: {
+        width: '80%',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#939393',
+        padding: 10,
+        backgroundColor: '#FFF',
+      },
+      card_content: {
+        marginTop: 20,
+        color: '#939393'
+      },
+      
+});
+
+export default Style;
